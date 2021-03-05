3fd1809b2ecfa07cc9a5e94135055173bd1c7689
augustonildo <augustocarvalhopp@gmail.com>
2021-03-05 19:49:53 -0300

    Implementação de heapSort e quickSort

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <sstream>
 
-#define ALGORITMO_ORDENACAO 1
+#define ALGORITMO_ORDENACAO 3
 
 // ALGORITMO_ORDENACAO  1
 void insertionSort(Planeta* planetas, int numeroPlanetas){
@@ -20,10 +20,79 @@ void insertionSort(Planeta* planetas, int numeroPlanetas){
 } 
 
 // ALGORITMO_ORDENACAO  2
-void mergeSort(Planeta* planetas, int numeroPlanetas){ }
+void refaz(int esquerda, int direita, Planeta *planetas){
+    int i = esquerda;
+    int j = i * 2;
+    Planeta aux = planetas[i];
+    while (j <= direita){
+        if (j < direita){
+            if (planetas[j].GetDistancia() < planetas[j+1].GetDistancia()){
+                j++;
+            }
+        }
+        if (aux.GetDistancia() >= planetas[j].GetDistancia()) {
+            break;
+        }
+        planetas[i] = planetas[j];
+        i = j;
+        j = i*2;
+    }
+    planetas[i] = aux;
+}
+
+void constroi(Planeta* planetas, int numeroPlanetas){
+    int esquerda = numeroPlanetas / 2;
+    while (esquerda >= 0) {
+        refaz(esquerda, numeroPlanetas-1, planetas);
+        esquerda--;
+    }
+}
+
+void heapSort(Planeta* planetas, int numeroPlanetas){
+    constroi(planetas, numeroPlanetas); 
+    int esquerda = 0; 
+    int direita = numeroPlanetas - 1;
+    Planeta aux;
+    while (direita >= 0)
+    {
+        aux = planetas[0];
+        planetas[0] = planetas[direita];
+        planetas[direita] = aux;
+        direita--;
+        refaz(esquerda, direita, planetas);
+    }
+}
 
 // ALGORITMO_ORDENACAO  3
-void quickSort(Planeta* planetas, int numeroPlanetas){ }
+void particao(int esquerda, int direita, int *i, int *j, Planeta *planetas){
+    *i = esquerda; 
+    *j = direita;
+    Planeta aux = planetas[(*i + *j)/2];
+    Planeta w;
+    do {
+        while (aux.GetDistancia() > planetas[*i].GetDistancia()) (*i)++;
+        while (aux.GetDistancia() < planetas[*j].GetDistancia()) (*j)--;
+        if (*i <= *j)
+        {
+            w = planetas[*i];
+            planetas[*i] = planetas[*j];
+            planetas[*j] = w;
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
+}
+
+void ordena(int esquerda, int direita, Planeta *planetas){
+    int i, j;
+    particao(esquerda, direita, &i, &j, planetas);
+    if (esquerda < j) ordena(esquerda, j, planetas);
+    if (i < direita) ordena(i, direita, planetas);
+}
+
+void quickSort(Planeta* planetas, int numeroPlanetas){
+    ordena(0, numeroPlanetas-1, planetas); 
+}
 
 // ALGORITMO_ORDENACAO  4
 void quickSortModificado(Planeta* planetas, int numeroPlanetas){ }
@@ -75,7 +144,7 @@ int main(int argc, char* argv[]) {
             insertionSort(planetas, numeroLinhas);
             break;
         case 2:
-            mergeSort(planetas, numeroLinhas);
+            heapSort(planetas, numeroLinhas);
             break;
         case 3:
             quickSort(planetas, numeroLinhas);
@@ -92,7 +161,7 @@ int main(int argc, char* argv[]) {
             return -1;
     }
     
-    for(int j = 0; j < 7; j++){
+    for(int j = numeroLinhas-1; j > numeroLinhas-8 && j >= 0; j--){
         cout << planetas[j].GetNome() << " " << planetas[j].GetDistancia() << endl; 
     }
 
